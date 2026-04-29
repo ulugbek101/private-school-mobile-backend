2e905ada78940bcf93e0871c7b38f3e09c15c8de
Ulug'bek Umaraliyev <thedevu101@gmail.com>
2026-04-29 15:52:11 +0500

    Add and configure django-cors-headers package;

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 
     'api',
 
@@ -35,6 +36,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -175,3 +179,5 @@ SIMPLE_JWT = {
 }
 
 LOGIN_URL = reverse_lazy("app_users:signin")
+
+CORS_ALLOW_ALL_ORIGINS = True
